fd4961bc092928bae18f71cb94c43c3b87e9d64d
iammoeezkhann <moeezk115@gmail.com>
2023-04-13 17:08:55 +0300

    Fixed hardcoded variables

--- oauth.py
+++ oauth.py
@@ -1,3 +1,4 @@
+import os
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from . import schemas
@@ -6,15 +7,14 @@ from fastapi.security import OAuth2PasswordBearer
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
-
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = os.getenv('ALGORITHM')
+ACCESS_TOKEN_EXPIRE_DAYS = os.getenv('ACCESS_TOKEN_EXPIRE_DAYS')
 
 
 def create_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(days=ACCESS_TOKEN_EXPIRE_DAYS)
     to_encode.update({"exp": expire})
 
     encoded = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
